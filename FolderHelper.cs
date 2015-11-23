680cd7672c4d6c0dfe6c9eff95ee49489c4eb81a
ziadkasmani <Ziad-PC@AppsCandle-Ziad.HEYVOTE>
2015-11-23 19:25:33 +0300

    abc
    abc

---
+++ FolderHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeyVoteClassLibrary.Helper
+{
+    public class FolderHelper
+    {
+        public string GetFolderName(string folderPath)
+        {
+            if (!String.IsNullOrEmpty(folderPath))
+            {
+                var folders = folderPath.Split('.');
+                if (folders.Length > 0 && folders[1]!=null)
+                    return folders[1];
+            }
+
+            throw new Exception(CodeHelper.UnableToFindFolder);
+        }
+
+    }
+}
